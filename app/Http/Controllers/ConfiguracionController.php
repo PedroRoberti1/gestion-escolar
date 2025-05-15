4ffbffb0e410f5d623d30c786e025c9c428c138e
Pedro Roberti <pedroroberti99@gmail.com>
2025-05-15 20:22:35 -0300

    terminamos la seccion de configuracion para agregar una nueva institucion a la base de datos

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -2,12 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Configuracion;
 use Illuminate\Http\Request;
 
 class ConfiguracionController extends Controller
 {
-    public function index(){
-         $configuracion = Configuracion::firts();
-        return view('admin.configuracion.index', compact('configuracion'));
+    public function index()
+    {
+
+        $jsonData = file_get_contents('https://api.hilariweb.com/divisas');
+        $divisas = json_decode($jsonData, true);
+        $configuracion = Configuracion::first();
+        return view('admin.configuracion.index', compact('configuracion', 'divisas'));
+    }
+
+    public function store(Request $request)
+    {
+
+
+        //Validacion de datos enviados por index.blade.php!
+        $request->validate([
+            'logo' => 'image|mimes:jpeg, png, jpg',
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'direccion' => 'required',
+            'telefono' => 'required',
+            'divisa' => 'required',
+            'correo_electronico' => 'required|email',
+
+        ]);
+
+        //BUSCAR si exits la configuracion(Si ya estan estos datos registrados)
+
+        $configuracion = Configuracion::first();
+
+        if ($configuracion) {
+            //ACTUALIZAR
+            $configuracion->nombre = $request->nombre;
+            $configuracion->descripcion = $request->descripcion;
+            $configuracion->direccion = $request->direccion;
+            $configuracion->telefono = $request->telefono;
+            $configuracion->divisa = $request->divisa;
+            $configuracion->correo_electronico = $request->correo_electronico;
+            $configuracion->web = $request->web;
+
+            if ($request->hasFile('logo')) {
+                //ELIMINAR EL LOGO ANTERIOR!
+                if ($configuracion->logo && file_exists(public_path($configuracion->logo))) {
+                    unlink(public_path($configuracion->logo));
+                }
+                //Guardar nuevo logo
+
+                $logoPath = $request->file('logo');
+                $nombreArchivo = time() . '_' . $logoPath->getClientOriginalName();
+                $rutaDestino = public_path('uploads/logos');
+                $logoPath->move($rutaDestino, $nombreArchivo);
+                $configuracion->logo = 'uploads/logos/' . $nombreArchivo;
+            }
+            $configuracion->save();
+            return redirect()->route('admin.configuracion.index')->with('success', 'La configuración se actualizó correctamente.');
+        } else {
+
+            //Crea una nueva configuracion!
+            $configuracion = new Configuracion();
+            $configuracion->nombre = $request->nombre;
+            $configuracion->descripcion = $request->descripcion;
+            $configuracion->direccion = $request->direccion;
+            $configuracion->telefono = $request->telefono;
+            $configuracion->divisa = $request->divisa;
+            $configuracion->correo_electronico = $request->correo_electronico;
+            $configuracion->web = $request->web;
+
+            if ($request->hasFile('logo')) {
+                //INGRESA UN LOGO
+                $logoPath = $request->file('logo');
+                $nombreArchivo = time() . '_' . $logoPath->getClientOriginalName();
+                $rutaDestino = public_path('uploads/logos');
+                $logoPath->move($rutaDestino, $nombreArchivo);
+                $configuracion->logo = 'uploads/logos/' . $nombreArchivo;
+            }
+
+            $configuracion->save();
+            return redirect()->route('admin.configuracion.index')->with('success', 'La configuración se actualizó correctamente.');
+        }
     }
 }
--- resources/views/admin/configuracion/index.blade.php
+++ resources/views/admin/configuracion/index.blade.php
@@ -9,30 +9,181 @@
 
 @section('content')
     <div class="row">
-    <div class="col-md-12">
+        <div class="col-md-12">
             <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Bienvenido a la seccion de configuracion del sistema.</h3>
-              </div>
-              <div class="card-body">
-                <form action="">
-                    <div class="row">
-                        <div class="col-md-4">
+                <div class="card-header">
+                    <h3 class="card-title">Bienvenido a la seccion de configuracion del sistema.</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.configuracion.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    
+                                    <label for="">Logo de la institucion</label><b> (*)</b>
+                                    <input type="file" onchange="mostrarImagen(event)" accept="image/*"
+                                        class="form-control" value="{{ old('logo', $configuracion->nombre ?? '') }}"
+                                        name="logo" placeholder="Escriba aqui.." required>
+                                    <br>
+                                    <center>
+                                        <img id="preview" src="{{url($configuracion->logo)}}" style="max-width: 200px; margin-top: 10px;">
+                                    </center>
+                                </div>
+                                <script>
+                                    const mostrarImagen = e =>
+                                        document.getElementById('preview').src = URL.createObjectURL(e.target.files[0]);
+                                </script>
+
+                                @error('logo')
+                                    <small style="color: red"> {{ $message }}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="">Nombre</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-university"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                    value="{{ old('nombre', $configuracion->nombre ?? '') }}" name="nombre"
+                                                    placeholder="Escriba aqui.." required>
+                                            </div>
+                                            @error('nombre')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="">Descripcion</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-university"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                    value="{{ old('descripcion', $configuracion->descripcion ?? '') }}"
+                                                    name="descripcion" placeholder="Escriba aqui.." required>
+                                            </div>
+                                            @error('descripcion')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Direccion</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-university"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                    value="{{ old('direccion', $configuracion->direccion ?? '') }}"
+                                                    name="direccion" placeholder="Escriba aqui.." required>
+                                            </div>
+                                            @error('direccion')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="">Telefono</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-phone"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                    value="{{ old('telefono', $configuracion->telefono ?? '') }}"
+                                                    name="telefono" placeholder="Escriba aqui.." required>
+                                            </div>
+                                            @error('telefono')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="">Divisa</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i
+                                                            class="fas fa-money-bill-wave"></i></span>
+                                                </div>
+                                                <select name="divisa" id="divisa" class="form-control" required>
+                                                    <option value="">Seleccione una opcion</option>
+                                                    @foreach ($divisas as $divisa)
+                                                        <option value="{{ $divisa['symbol'] }}"
+                                                            {{ old('divisa', $configuracion->divisa ?? '') == $divisa['symbol'] ? 'selected' : '' }}>
+                                                            {{ $divisa['name'] . '(' . $divisa['symbol'] . ')' }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            @error('divisa')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Correo Electronico</label><b> (*)</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-envelope"></i></span>
+                                                </div>
+                                                <input type="correo_electronico" class="form-control"
+                                                    value="{{ old('correo_electronico', $configuracion->correo_electronico ?? '') }}"
+                                                    name="correo_electronico" placeholder="Escriba aqui.." required>
+                                            </div>
+                                            @error('correo_electronico')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Sitio Web</label><b> ()</b>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"> <i class="fas fa-globe"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                    value="{{ old('web', $configuracion->web ?? '') }}" name="web"
+                                                    placeholder="Escriba aqui...">
+                                            </div>
+                                            @error('web')
+                                                <small style="color: red"> {{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <a href="{{ url('/admin') }}" class="btn btn-default">
+                                        <i class="fas fa-arrow-left"></i> Cancelar
+                                    </a>
+                                    <button type="submit" class="btn btn-primary"> <i
+                                            class="fas fa-save"></i>Guardar</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-8">
-                        <p>dwqdwqd</p>
-                        </div>
-                    </div>
-                </form>
-              </div>
-              <!-- /.card-body -->
+                    </form>
+                </div>
+                <!-- /.card-body -->
             </div>
             <!-- /.card -->
         </div>
@@ -46,5 +197,7 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-@stop 
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
+@stop
